77c4e614d03d8947835974cd7f01987eb60e7aa7
Reni Koci <56065739+AcronRK@users.noreply.github.com>
2024-03-31 11:00:06 +0200

    still trying...

--- src/components/OurCoaches/OurCoaches.tsx
+++ src/components/OurCoaches/OurCoaches.tsx
@@ -1,34 +1,36 @@
-import Card from './Card'
+import Card from './Card';
 
 const OurCoaches = () => {
   return (
-    <section id="ourcoaches" className="mx-auto w-5/6 items-center justify-center py-24">
-      <Card 
-        videoSide="left"
-        player="Faker"
-        title="The Unkillable Demon"
-        description='Teaching you how to play on top of your games - simultaneously'
-        url="https://www.youtube.com/watch?v=cSpLzG0pvk4"
-      />
+    <section id="ourcoaches" className="mx-auto w-5/6 items-center justify-center py-24 overflow-x-hidden">
+      {/* Add overflow-x-hidden class to the section */}
+      <div className="flex flex-col md:flex-row overflow-x-hidden">
+        <Card 
+          videoSide="left"
+          player="Faker"
+          title="The Unkillable Demon"
+          description='Teaching you how to play on top of your games - simultaneously'
+          url="https://www.youtube.com/watch?v=cSpLzG0pvk4"
+        />
 
-      <Card 
-        videoSide="right"
-        player="Tyler1"
-        title="Your Average Teammate"
-        description='The best player to learn respect and collaboreation from. You might also get some gym tips while you are at it!'
-        url="https://www.youtube.com/watch?v=0hRaGXWim2k"
-      />
+        <Card 
+          videoSide="right"
+          player="Tyler1"
+          title="Your Average Teammate"
+          description='The best player to learn respect and collaboration from. You might also get some gym tips while you are at it!'
+          url="https://www.youtube.com/watch?v=0hRaGXWim2k"
+        />
 
-      <Card 
-        videoSide="left"
-        player="Kesha"
-        title="The Sanest Nunu Player"
-        description="We're gonna let the clip do the talking on this one "
-        url="https://www.youtube.com/watch?v=jGEVB6lbcGM"
-      />
-      
+        <Card 
+          videoSide="left"
+          player="Kesha"
+          title="The Sanest Nunu Player"
+          description="We're gonna let the clip do the talking on this one "
+          url="https://www.youtube.com/watch?v=jGEVB6lbcGM"
+        />
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default OurCoaches
+export default OurCoaches;
--- src/components/WhyUs/WhyUs.tsx
+++ src/components/WhyUs/WhyUs.tsx
@@ -19,15 +19,15 @@ const WhyUs = () => {
           {/* Safe and Secure */}
           <div className="flex flex-col justify-content items-center">
             <MagnifyingGlassCircleIcon className="h-20 w-20 text-white"/>
-            <h1 className="font-bold text-red-500 text-3xl pt-2 pb-3">EASY BOOKING</h1>
-            <p className="md:text-lg">Sessions are quickly and securely booked through our easy-to-use coaching portal. </p>
+            <h1 className="font-bold text-red-500 text-3xl pt-2 pb-3">SAFE AND SECURE</h1>
+            <p className="md:text-lg">We are not to blame for ear damage, eye problems or depression. That's on you pal.</p>
           </div>
 
           {/* Better KDA in no time */}
           <div className="flex flex-col justify-content items-center">
             <ArrowUpIcon className="h-20 w-20 text-white"/>
-            <h1 className="font-bold text-red-500 text-3xl pt-2 pb-3">EASY BOOKING</h1>
-            <p className="md:text-lg">Sessions are quickly and securely booked through our easy-to-use coaching portal. </p>
+            <h1 className="font-bold text-red-500 text-3xl pt-2 pb-3">BETTER KDA</h1>
+            <p className="md:text-lg">If you are not seeing results, that just means Tyler1 wasn't screaming enough. It's our fault and we take full responsability.</p>
           </div>
           </div>
           
